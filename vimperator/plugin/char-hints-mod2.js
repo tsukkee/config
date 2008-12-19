3ec5f9303c52633a5f494d97dd73f390340861f1
tsukkee <takayuki0510@gmail.com>
2008-12-19 21:38:44 +0900

    update vimperatorrc and vimperator plugin

--- vimperator/plugin/char-hints-mod2.js
+++ vimperator/plugin/char-hints-mod2.js
@@ -4,17 +4,24 @@ var PLUGIN_INFO =
     <name>{NAME}</name>
     <description>character hint mode.</description>
     <author mail="konbu.komuro@gmail.com" homepage="http://d.hatena.ne.jp/hogelog/">hogelog</author>
-    <version>0.0.1</version>
+    <version>0.0.2</version>
     <minVersion>2.0pre 2008/12/12</minVersion>
-    <maxVersion>2.0pre</maxVersion>
+    <maxVersion>2.0a1</maxVersion>
     <detail><![CDATA[
 
 == Usage ==
-LowerCase => input hint command line.
-UpperCase => select char-hint label.
+In default setting,
+input quickhint in lowercase
+and
+select charhint label in uppercase.
 
 == OPTIONS ==
-set histchars="hjkl" => show char-hint use h, j, k, l.
+set histchars="hjkl":
+    show char-hint use h, j, k, l.
+set charhintinput=uppercase|lowercase:
+    charhint input in uppercase|lowercase
+set charhintshow=uppercase|lowercase:
+    charhint show in uppercase|lowercase
 
 == TODO ==
  * support hinttimeout.
@@ -22,11 +29,17 @@ set histchars="hjkl" => show char-hint use h, j, k, l.
     <detail lang="ja"><![CDATA[
 
 == Usage ==
-小文字は候補を絞るためのテキスト入力に使います。
+デフォルトの設定では
+小文字は候補を絞るためのテキスト入力に、
 大文字は文字ラベルの選択に使います。
 
 == OPTIONS ==
-set histchars="hjkl" => show char-hint use h, j, k, l.
+set histchars="hjkl":
+    show char-hint use h, j, k, l.
+set charhintinput=uppercase|lowercase:
+    charhint input in uppercase|lowercase
+set charhintshow=uppercase|lowercase:
+    charhint show in uppercase|lowercase
 
 == TODO ==
  * support hinttimeout.
@@ -38,15 +51,67 @@ set histchars="hjkl" => show char-hint use h, j, k, l.
 
     const DEFAULT_HINTCHARS = "HJKLASDFGYUIOPQWERTNMZXCVB";
 
-    options.add(["hintchars", "hchar"],
+    let inputCase = function(str) str.toUpperCase();
+    let inputRegex = /[A-Z]/;
+    let showCase = function(str) str.toUpperCase();
+
+    options.add(["hintchars", "hchar"], //{{{
         "Hint characters",
-        "string", DEFAULT_HINTCHARS);
+        "string", DEFAULT_HINTCHARS); //}}}
+    options.add(["charhintinput", "chinput"], //{{{
+        "Character Hint Input",
+        "string", "uppercase",
+        {
+            setter: function (value)
+            {
+                switch (value)
+                {
+                    default:
+                    case "uppercase":
+                        inputCase = function(str) str.toUpperCase();
+                        inputRegex = /[A-Z]/;
+                        break;
+                    case "lowercase":
+                        inputCase = function(str) str.toLowerCase();
+                        inputRegex = /[a-z]/;
+                        break;
+                }
+            },
+            completer: function () [
+                ["uppercase", "Input charhint UpperCase"],
+                ["lowercase", "Input charhint LowerCase"],
+            ],
+        }); //}}}
+    options.add(["charhintshow", "chshow"], //{{{
+        "Character Hint Show",
+        "string", "uppercase",
+        {
+            setter: function (value)
+            {
+                switch (value)
+                {
+                    default:
+                    case "uppercase":
+                        showCase = function(str) str.toUpperCase();
+                        break;
+                    case "lowercase":
+                        showCase = function(str) str.toLowerCase();
+                        break;
+                }
+            },
+            completer: function () [
+                ["uppercase", "show charhint UpperCase"],
+                ["lowercase", "show charhint LowerCase"],
+            ],
+        }); //}}}
+        
 
     function chars2num(chars) //{{{
     {
-        var num = 0;
-        var hintchars = options.hintchars.toUpperCase();
-        var base = hintchars.length;
+        let num = 0;
+        //let hintchars = options.hintchars.toUpperCase();
+        let hintchars = inputCase(options.hintchars);
+        let base = hintchars.length;
         for(let i=0,l=chars.length;i<l;++i) {
             num = base*num + hintchars.indexOf(chars[i]);
         }
@@ -54,9 +119,10 @@ set histchars="hjkl" => show char-hint use h, j, k, l.
     } //}}}
     function num2chars(num) //{{{
     {
-        var chars = "";
-        var hintchars = options.hintchars.toUpperCase();
-        var base = hintchars.length;
+        let chars = "";
+        //let hintchars = options.hintchars.toUpperCase();
+        let hintchars = inputCase(options.hintchars);
+        let base = hintchars.length;
         do {
             chars = hintchars[((num % base))] + chars;
             num = Math.floor(num/base);
@@ -73,7 +139,8 @@ set histchars="hjkl" => show char-hint use h, j, k, l.
         {
             let num = elem.getAttribute("number");
             let hintchar = num2chars(parseInt(num, 10));
-            elem.setAttribute("hintchar", hintchar);
+            //elem.setAttribute("hintchar", hintchar);
+            elem.setAttribute("hintchar", showCase(hintchar));
         }
         Array.forEach(win.frames, showCharHints);
     } //}}}
@@ -89,25 +156,40 @@ set histchars="hjkl" => show char-hint use h, j, k, l.
         }, //}}}
         onInput: function (event) //{{{
         {
-            var hintString = commandline.command;
-            commandline.command = hintString.replace(/[A-Z]+/g, "");
+            let eventkey = events.toString(event);
+            if(/^\d$/.test(eventkey)) {
+                commandline.command += eventkey;
+            }
+            let hintString = commandline.command;
+            commandline.command = hintString.replace(inputRegex, "");
+            //commandline.command = hintString.replace(/[A-Z]+/g, "");
             charhints.original.onInput(event);
             showCharHints();
             for(let i=0,l=hintString.length;i<l;++i) {
-                if(/^[A-Z]$/.test(hintString[i])) {
+                //if(/^[A-Z]$/.test(hintString[i])) {}
+                if(inputRegex.test(hintString[i])) {
                     hintChars.push(hintString[i]);
                 }
             }
             if(hintChars.length>0) {
                 let numstr = String(chars2num(hintChars.join("")));
+                // no setTimeout, don't run nice
                 setTimeout(function () {
                     for(let i=0,l=numstr.length;i<l;++i) {
                         let num = numstr[i];
                         let alt = new Object;
                         alt.liberatorString = num;
-                        hints.onEvent(alt);
+                        charhints.original.onEvent(alt);
                     }
                 }, 10);
+            }
+        }, //}}}
+        onEvent: function (event) //{{{
+        {
+            if(/^\d$/.test(events.toString(event))) {
+                charhints.onInput(event);
+            } else {
+                charhints.original.onEvent(event);
             }
         }, //}}}
     };
@@ -116,30 +198,27 @@ set histchars="hjkl" => show char-hint use h, j, k, l.
         charhints.original = {
             show: hints.show,
             onInput: liberator.eval("onInput", hintContext),
+            onEvent: hints.onEvent,
         };
 
         charhints.install = function () //{{{
         {
             hints.show = charhints.show;
+            hints.onEvent = charhints.onEvent;
             liberator.eval("onInput = plugins.charhints.onInput", hintContext);
 
-            highlight.CSS = highlight.CSS.replace(
-                    "Hint::after,,*  content: attr(number);",
-                    "Hint::after,,*  content: attr(hintchar);");
-            highlight.reload();
+            liberator.execute(":hi Hint::after content: attr(hintchar)");
         }; //}}}
         charhints.uninstall = function () //{{{
         {
             hints.show = charhints.original.show;
+            hints.onEvent = charhints.original.onEvent;
             liberator.eval("onInput = plugins.charhints.original.onInput", hintContext);
 
-            highlight.CSS = highlight.CSS.replace(
-                    "Hint::after,,*  content: attr(hintchar);",
-                    "Hint::after,,*  content: attr(number);");
-            highlight.reload();
+            liberator.execute(":hi Hint::after content: attr(number)");
         }; //}}}
     }
     charhints.install();
 })();
 
-// vim: set fdm=marker sw=4 ts=4 et:
+// vim: set fdm=marker sw=4 ts=4 et fenc=utf-8:
