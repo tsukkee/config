66ad85ffa2bc839e9586ebe3989aebf295e86b1e
tsukkee <takayuki0510@gmail.com>
2022-10-06 09:36:44 +0900

    disable closing shell by ctrl-d

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,9 @@ function fish_user_key_bindings -d "Define user key bindings"
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
     bind -M insert \cf accept-autosuggestion
+    bind -M insert \cd delete-char
 
     bind u undo
     bind \cr redo
+    bind \cd delete-char
 end
