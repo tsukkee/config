55cc95de4da5a2c71c114cd51d07c38c7f4a6c6f
tsukkee <takayuki0510@gmail.com>
2023-01-25 11:51:45 +0900

    update fish config

--- fish/functions/fish_right_prompt.fish
+++
@@ -1,3 +0,0 @@
-# function fish_right_prompt -d "Write out the right prompt"
-#     fish_git_prompt
-# end
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings -d "Define user key bindings"
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
+    bind -M insert \cr history-pager
     bind -M insert \cf accept-autosuggestion
     bind -M insert \cd delete-char
 
