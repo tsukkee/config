20f6a55487594cda8df1602d6dcc3d1561262012
tsukkee <takayuki0510@gmail.com>
2023-06-01 17:12:29 +0900

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,21 +1,28 @@
 local wezterm = require 'wezterm'
 
 return {
-    font = wezterm.font("PlemolJP Console NF"),
+    font = wezterm.font_with_fallback({
+        { family = "0xProto" },
+        { family = "PlemolJP Console NF" },
+        { family = 'Noto Emoji', assume_emoji_presentation = true},
+    }),
     use_ime = true,
     use_dead_keys = false,
-    font_size = 19.0,
+    macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
+    font_size = 18.0,
     color_scheme = "nord",
     keys = {
         {key="¥",  action=wezterm.action.SendKey { key = '\\' }}
     },
     enable_tab_bar = false,
     window_padding = {
-        left = 0,
+        left = 12,
         right = 0,
         top = 0,
         bottom = 0,
     },
+    term = 'wezterm',
+    audible_bell = 'Disabled',
     -- debug_key_events = true
 }
 
